96c960ee0e4928fda25b7fb6e1ba887416caf19e
passt0r <dima3@ymail.com>
2018-04-11 18:36:21 +0300

    Complete chapter 11

--- layer_animations/BahamaAirLoginScreen/BahamaAirLoginScreen/ViewController.swift
+++ layer_animations/BahamaAirLoginScreen/BahamaAirLoginScreen/ViewController.swift
@@ -28,19 +28,31 @@ func delay(seconds: Double, completion: @escaping ()-> Void) {
 }
 
 func tintBackgroundColor(layer: CALayer, toColor: UIColor) {
-    let colorAnimation = CABasicAnimation(keyPath: "backgroundColor")
+    let colorAnimation = CASpringAnimation(keyPath: "backgroundColor")
     colorAnimation.fromValue = layer.backgroundColor
     colorAnimation.toValue = toColor.cgColor
-    colorAnimation.duration = 1.0
+    
+    colorAnimation.damping = 28.0
+    colorAnimation.initialVelocity = 2.0
+    colorAnimation.mass = 3.0
+    colorAnimation.stiffness = 600.0
+    
+    colorAnimation.duration = colorAnimation.settlingDuration
     layer.add(colorAnimation, forKey: nil)
     layer.backgroundColor = toColor.cgColor
 }
 
 func roundCorners(layer: CALayer, toRadius: CGFloat) {
-    let roundAnimation = CABasicAnimation(keyPath: "cornerRadius")
+    let roundAnimation = CASpringAnimation(keyPath: "cornerRadius")
     roundAnimation.fromValue = layer.cornerRadius
     roundAnimation.toValue = toRadius
-    roundAnimation.duration = 0.33
+    
+    roundAnimation.damping = 28.0
+    roundAnimation.initialVelocity = 2.0
+    roundAnimation.mass = 3.0
+    roundAnimation.stiffness = 600.0
+    
+    roundAnimation.duration = roundAnimation.settlingDuration
     layer.add(roundAnimation, forKey: nil)
     layer.cornerRadius = toRadius
 }
@@ -333,11 +345,12 @@ extension ViewController: CAAnimationDelegate {
         if name == "form",  let layer = anim.value(forKey: "layer") as? CALayer {
             anim.setValue(nil, forKey: "layer")
             
-            let pusle = CABasicAnimation(keyPath: "transform.scale")
-            pusle.fromValue = 1.25
-            pusle.toValue = 1.0
-            pusle.duration = 0.25
-            layer.add(pusle, forKey: nil)
+            let pulse = CASpringAnimation(keyPath: "transform.scale")
+            pulse.damping = 7.5
+            pulse.fromValue = 1.25
+            pulse.toValue = 1.0
+            pulse.duration = pulse.settlingDuration
+            layer.add(pulse, forKey: nil)
         }
         
         if name == "cloud", let layer = anim.value(forKey: "layer") as? CALayer {
@@ -354,6 +367,35 @@ extension ViewController: CAAnimationDelegate {
 extension ViewController: UITextFieldDelegate {
     func textFieldDidBeginEditing(_ textField: UITextField) {
         info.layer.removeAnimation(forKey: "infoappear")
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text else { return }
+        if text.count < 5 {
+            let jump = CASpringAnimation(keyPath: "position.y")
+            jump.fromValue = textField.layer.position.y + 1.0
+            jump.toValue = textField.layer.position.y
+            
+            jump.initialVelocity = 100.0
+            jump.mass = 10.0
+            jump.stiffness = 1500.0
+            jump.damping = 50.0
+            
+            jump.duration = jump.settlingDuration
+            textField.layer.add(jump, forKey: nil)
+            
+            textField.layer.borderWidth = 3.0
+            textField.layer.borderColor = UIColor.clear.cgColor
+            
+            let flash = CASpringAnimation(keyPath: "borderColor")
+            flash.damping = 7.0
+            flash.stiffness = 200.0
+            flash.fromValue = UIColor(red: 1.0, green: 0.27, blue: 0.0, alpha:
+                1.0).cgColor
+            flash.toValue = UIColor.white.cgColor
+            flash.duration = flash.settlingDuration
+            textField.layer.add(flash, forKey: nil)
+        }
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
